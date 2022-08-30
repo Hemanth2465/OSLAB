108c81a790265664176e79f7e677a43cc271d0d2
Hemanth2465 <111737771+Hemanth2465@users.noreply.github.com>
2022-08-30 11:54:21 +0530

    hemanth completed sample.c with C source Code

---
+++ sample.c
@@ -0,0 +1,12 @@
+# CNOSLAB
+/* Name Of the Candidate: L. Ashok
+   Roll No: 204G1A0501
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 29-08-2022
+   Date of Execution: 29-08-2022
+*/
+ // Source Code
+    #include <stdio.h>
+    int main(){
+	printf("Hello World\n");
+ }
